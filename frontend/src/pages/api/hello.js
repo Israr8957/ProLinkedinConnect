af4ff517ea114625bcaa8656d650005c010748c2
Israr8957 <ik6141333@gmail.com>
2025-07-28 02:10:27 +0530

    deploying frontend

--- frontend/src/pages/api/hello.js
+++ frontend/src/pages/api/hello.js
@@ -1,9 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
+// export default function handler(req, res) {
+//   res.status(200).json({ name: "John Doe" });
+// }
 
-
-PASSWORD:lD9OICY03Sn6SLb9
-MONGO_URI:mongodb+srv://ik6141333:lD9OICY03Sn6SLb9@cluster0.9qgjl1v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// PASSWORD:lD9OICY03Sn6SLb9
+// MONGO_URI:mongodb+srv://ik6141333:lD9OICY03Sn6SLb9@cluster0.9qgjl1v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
